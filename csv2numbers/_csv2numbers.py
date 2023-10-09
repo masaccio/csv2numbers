04a59425082690ebedf49a933c609da22d5f71ca
Jon Connell <python@figsandfudge.com>
2023-10-09 19:26:57 +0100

    Refactor converter as a class

--- csv2numbers/_csv2numbers.py
+++ csv2numbers/_csv2numbers.py
@@ -5,6 +5,7 @@ import argparse
 import csv
 import re
 import warnings
+from dataclasses import dataclass
 from pathlib import Path
 from sys import exit, stderr
 from typing import NamedTuple, Tuple  # noqa: F401
@@ -23,28 +24,89 @@ class ColumnTransform(NamedTuple):
     func: callable
 
 
-def rename_columns(data: pd.DataFrame, mapper: dict) -> pd.DataFrame:
-    """Rename columns using column map."""
-    if mapper is None:
-        return data
-    return data.rename(columns=mapper)
+@dataclass
+class Converter:
+    input_filename: str = None
+    output_filename: str = None
+    date_columns: list = None
+    day_first: bool = False
+    no_header: bool = False
+    reverse: bool = False
+    whitespace: bool = None
 
+    def __post_init__(self: Converter) -> None:
+        """Parse CSV file with Pandas and return a dataframe."""
+        header = None if self.no_header else 0
+        parse_dates = self.date_columns if self.date_columns is not None else False
+        try:
+            with warnings.catch_warnings():
+                warnings.simplefilter(action="ignore", category=UserWarning)
+                self.data = pd.read_csv(
+                    self.input_filename,
+                    dayfirst=self.day_first,
+                    header=header,
+                    parse_dates=parse_dates,
+                    thousands=",",
+                )
+        except FileNotFoundError as e:
+            msg = f"{self.input_filename}: file not found"
+            raise RuntimeError(msg) from e
+        except pd.errors.ParserError as e:
+            msg = f"{self.input_filename}: {e.args[0]}"
+            raise RuntimeError(msg) from e
+        else:
+            self.data = self.data.fillna("")
+            for column in self.data.columns:
+                if self.whitespace:
+                    self.data[column] = self.data[column].apply(func=filter_whitespace)
 
-def delete_columns(data: pd.DataFrame, columns: list) -> pd.DataFrame:
-    """Delete columns from the data."""
-    if columns is None:
-        return data
-    try:
-        index_to_name = dict(enumerate(data.columns))
-        columns_to_delete = [
-            index_to_name[x] if isinstance(x, int) else x for x in columns
-        ]
-        data = data.drop(columns=columns_to_delete)
-    except KeyError:
-        msg = "'" + "', '".join([str(x) for x in columns]) + "'"
-        msg += ": cannot delete: column(s) do not exist in CSV"
-        raise RuntimeError(msg) from None
-    return data
+            if self.reverse:
+                self.data = self.data.iloc[::-1]
+                self.data = self.data.reset_index(drop=True)
+
+    def rename_columns(self: Converter, mapper: dict) -> None:
+        """Rename columns using column map."""
+        if mapper is None:
+            return
+        self.data = self.data.rename(columns=mapper)
+
+    def delete_columns(self: Converter, columns: list) -> None:
+        """Delete columns from the data."""
+        if columns is None:
+            return
+
+        try:
+            index_to_name = dict(enumerate(self.data.columns))
+            columns_to_delete = [
+                index_to_name[x] if isinstance(x, int) else x for x in columns
+            ]
+            self.data = self.data.drop(columns=columns_to_delete)
+        except KeyError:
+            msg = "'" + "', '".join([str(x) for x in columns]) + "'"
+            msg += ": cannot delete: column(s) do not exist in CSV"
+            raise RuntimeError(msg) from None
+
+    def transform_columns(self: Converter, columns: list[ColumnTransform]) -> None:
+        """Perform column transformationstransformations."""
+        if columns is None:
+            return
+        for transform in columns:
+            self.data = transform.func(self.data, transform.source, transform.dest)
+
+    def __del__(self: Converter) -> None:
+        """Write dataframe transctions to a Numbers file."""
+        doc = Document(num_rows=2, num_cols=2)
+        table = doc.sheets[0].tables[0]
+
+        for col_num, value in enumerate(self.data.columns.tolist()):
+            table.write(0, col_num, value)
+
+        for row_num, row in self.data.iterrows():
+            for col_num, value in enumerate(row):
+                if value:
+                    table.write(row_num + 1, col_num, value)
+
+        doc.save(self.output_filename)
 
 
 def col_names_for_transform(row: pd.Series, source: str, dest: str) -> tuple[str, str]:
@@ -105,38 +167,11 @@ def pos_transform(data: pd.DataFrame, source: str, dest: str) -> pd.DataFrame:
     return data.apply(lambda row: positive_values(row, source, dest), axis=1)
 
 
-def transform_columns(
-    data: pd.DataFrame, columns: list[ColumnTransform]  # noqa: COM812
-) -> pd.DataFrame:
-    """Perform column transformationstransformations."""
-    if columns is None:
-        return data
-    for transform in columns:
-        data = transform.func(data, transform.source, transform.dest)
-    return data
-
-
 def filter_whitespace(x: str) -> str:
     """Strip and collapse whitespace."""
     if isinstance(x, str):
         return re.sub(r"\s+", " ", x.strip())
     return x
-
-
-def reformat_data(
-    data: pd.DataFrame, *, whitespace: bool, reverse: bool  # noqa: COM812
-) -> pd.DataFrame:
-    """Apply data corrections to tabls."""
-    data = data.fillna("")
-    for column in data.columns:
-        if whitespace:
-            data[column] = data[column].apply(func=filter_whitespace)
-
-    if reverse:
-        data = data.iloc[::-1]
-        data = data.reset_index(drop=True)
-
-    return data
 
 
 def parse_columns(arg: str) -> list:
@@ -255,49 +290,6 @@ def command_line_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def read_csv_file(
-    filename: str,
-    *,
-    day_first: bool,
-    no_header: bool,
-    date_columns: list,
-) -> pd.DataFrame:
-    """Parse CSV file with Pandas and return a dataframe."""
-    header = None if no_header else 0
-    parse_dates = date_columns if date_columns is not None else False
-    try:
-        data = pd.read_csv(
-            filename,
-            dayfirst=day_first,
-            header=header,
-            parse_dates=parse_dates,
-            thousands=",",
-        )
-    except FileNotFoundError as e:
-        msg = f"{filename}: file not found"
-        raise RuntimeError(msg) from e
-    except pd.errors.ParserError as e:
-        msg = f"{filename}: {e.args[0]}"
-        raise RuntimeError(msg) from e
-    return data
-
-
-def write_output(data: pd.DataFrame, filename: str) -> None:
-    """Write dataframe transctions to a Numbers file."""
-    doc = Document(num_rows=2, num_cols=2)
-    table = doc.sheets[0].tables[0]
-
-    for col_num, value in enumerate(data.columns.tolist()):
-        table.write(0, col_num, value)
-
-    for row_num, row in data.iterrows():
-        for col_num, value in enumerate(row):
-            if value:
-                table.write(row_num + 1, col_num, value)
-
-    doc.save(filename)
-
-
 def main() -> None:
     """Convert the document and exit."""
     parser = command_line_parser()
@@ -321,21 +313,20 @@ def main() -> None:
         exit(1)
 
     try:
-        for csv_filename, output_filename in zip(args.csvfile, output_filenames):
-            with warnings.catch_warnings():
-                warnings.simplefilter(action="ignore", category=UserWarning)
-                data = read_csv_file(
-                    csv_filename,
-                    day_first=args.day_first,
-                    no_header=args.no_header,
-                    date_columns=args.date,
-                )
-            data = reformat_data(data, whitespace=args.whitespace, reverse=args.reverse)
-            data = transform_columns(data, args.transform)
-            data = rename_columns(data, args.rename)
-            data = delete_columns(data, args.delete)
+        for input_filename, output_filename in zip(args.csvfile, output_filenames):
+            converter = Converter(
+                day_first=args.day_first,
+                no_header=args.no_header,
+                whitespace=args.whitespace,
+                reverse=args.reverse,
+                date_columns=args.date,
+                input_filename=input_filename,
+                output_filename=output_filename,
+            )
 
-            write_output(data, output_filename)
+            converter.transform_columns(args.transform)
+            converter.rename_columns(args.rename)
+            converter.delete_columns(args.delete)
     except RuntimeError as e:
         print(e, file=stderr)
         exit(1)
